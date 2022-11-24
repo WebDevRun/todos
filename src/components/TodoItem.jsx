63a391435ed7655b6d39d14a4b32a877b662daf7
leonid <leonkaram@mail.ru>
2022-11-24 18:19:52 +0700

    create visual for status

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,26 +1,50 @@
-import { useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 import { DeleteTodoForm } from './DeleteTodoForm'
 import { UpdateTodoForm } from './UpdateTodoForm'
+import { Error } from './Error'
 import { ref as refDB, update } from 'firebase/database'
 import { database } from '../firebase'
 
 export function TodoItem({ todo }) {
   const [openDeleteModal, setOpenDeleteModal] = useState(false)
   const [openUpdateModal, setOpenUpdateModal] = useState(false)
+  const [error, setError] = useState(null)
+  const articleRef = useRef()
   const checkboxOnChangeHandler = () => {
-    update(refDB(database, `todos/${todo.id}`), {
-      ...todo,
-      complite: !todo.complite,
-    })
+    try {
+      update(refDB(database, `todos/${todo.id}`), {
+        ...todo,
+        complite: !todo.complite,
+      })
+    } catch (error) {
+      setError(error.message)
+    }
   }
   const updateButtonClickHandler = () => setOpenUpdateModal((prev) => !prev)
   const deleteButtonClickHandler = () => setOpenDeleteModal((prev) => !prev)
 
+  useEffect(() => {
+    const nowDate = Date.now()
+    const endDate = new Date(todo.endDate)
+    if (nowDate > endDate && !todo.complite)
+      return articleRef.current.classList.add('item_expired')
+    articleRef.current.classList.remove('item_expired')
+  }, [todo.endDate, todo.complite])
+
+  useEffect(() => {
+    if (todo.complite) articleRef.current.classList.add('item_ready')
+    else articleRef.current.classList.remove('item_ready')
+  }, [todo.complite])
+
+  const endDateTime = useMemo(() => {
+    return new Date(todo.endDate).toLocaleString()
+  }, [todo.endDate])
+
   return (
     <>
-      <article className="todoList__item">
+      <article ref={articleRef} className={`todoList__item`}>
         <h2 className="todoList__title">{todo.title}</h2>
-        <p className="todoList__endDate">{todo.endDate}</p>
+        <p className="todoList__endDate">Дата завершения: {endDateTime}</p>
         <p className="todoList__description">{todo.description}</p>
         {todo.uploadFilesData?.length && (
           <div className="todoList__files">
@@ -41,7 +65,7 @@ export function TodoItem({ todo }) {
               checked={todo.complite}
               onChange={checkboxOnChangeHandler}
             />
-            Статус: {todo.complite ? 'завершен' : 'не завершен'}
+            <p>Статус: {todo.complite ? 'завершен' : 'не завершен'}</p>
           </label>
         </div>
         <div className="todoList__controls">
@@ -58,6 +82,7 @@ export function TodoItem({ todo }) {
             onClick={deleteButtonClickHandler}
           />
         </div>
+        {error && <Error errorMessage={error} />}
       </article>
       {openUpdateModal && (
         <UpdateTodoForm
